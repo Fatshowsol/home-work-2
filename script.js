e8f5cc6249c307393cf5dc285d165502ef35757c
Andrii <example@gmail.com>
2019-08-26 19:05:42 +0200

    add loop while

--- script.js
+++ script.js
@@ -12,7 +12,25 @@ let appData = {
 
 
 
-for (let i =0; i < 2; i++) {
+//for (let i = 0; i < 2; i++) {
+//    let a = prompt('Введите обязательную статью расходов в этом месяце'),
+//        b = prompt('Во сколько обойдется?');
+    
+//    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+//        && a != '' && b != '' && a.length < 50 ) {
+//        console.log('done');
+//        appData.expenses[a] = b;
+//    } else {
+//        console.log('Некоректные данные. Попробуйте ещё раз');
+//        i--;
+//    }
+//};
+
+// Цикл while
+
+let i = 0;
+
+while (i < 2) {
     let a = prompt('Введите обязательную статью расходов в этом месяце'),
         b = prompt('Во сколько обойдется?');
     
@@ -21,9 +39,12 @@ for (let i =0; i < 2; i++) {
         console.log('done');
         appData.expenses[a] = b;
     } else {
-
+        console.log('Некоректные данные. Попробуйте ещё раз');
+        i--;
     }
-};
+    i++;
+}
+  
 
 appData.moneyPerDay = appData.budget / 30;
 
