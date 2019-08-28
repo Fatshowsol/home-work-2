e9b165c44a5a5b1987d9022a34d15948c16641dc
Andrii <example@gmail.com>
2019-08-28 21:47:19 +0200

    add do...while loop

--- script.js
+++ script.js
@@ -12,23 +12,23 @@ let appData = {
 
 
 
-//for (let i = 0; i < 2; i++) {
-//    let a = prompt('Введите обязательную статью расходов в этом месяце'),
-//        b = prompt('Во сколько обойдется?');
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Введите обязательную статью расходов в этом месяце'),
+        b = prompt('Во сколько обойдется?');
     
-//    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-//        && a != '' && b != '' && a.length < 50 ) {
-//        console.log('done');
-//        appData.expenses[a] = b;
-//    } else {
-//        console.log('Некоректные данные. Попробуйте ещё раз');
-//        i--;
-//    }
-//};
+    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+        && a != '' && b != '' && a.length < 50 ) {
+        console.log('done');
+        appData.expenses[a] = b;
+    } else {
+        console.log('Некоректные данные. Попробуйте ещё раз');
+        i--;
+    }
+}
 
 // Цикл while
 
-let i = 0;
+/*let i = 0;
 
 while (i < 2) {
     let a = prompt('Введите обязательную статью расходов в этом месяце'),
@@ -43,8 +43,25 @@ while (i < 2) {
         i--;
     }
     i++;
-}
-  
+}*/
+
+// цикл do...while
+
+/*do {
+    
+    let a = prompt('Введите обязательную статью расходов в этом месяце'),
+    b = prompt('Во сколько обойдется?');
+
+    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+        && a != '' && b != '' && a.length < 50 ) {
+        console.log('done');
+        appData.expenses[a] = b;
+    } else {
+        console.log('Некоректные данные. Попробуйте ещё раз');
+        i--;
+    }
+    i++;
+} while (i < 2);*/
 
 appData.moneyPerDay = appData.budget / 30;
 
